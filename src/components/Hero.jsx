a46bc59a63b71ccf5cafed9bedfd8df34ec81798
sboualouchi7 <salmanboualouchi@gmail.com>
2025-03-05 12:28:18 +0100

    secend commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, Im <span className="text-[#7f4ded]">Adam</span>
+            Hi, Im <span className="text-[#7f4ded]">Salman</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop full stack web applications{" "}
